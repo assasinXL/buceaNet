796146e3e161fab53c71430b4714032e5721ea53
assasinXL <yyzyuming@gmail.com>
2021-01-02 02:28:27 +0800

    fix -2021-01-02

--- cpp/src/main.cc
+++ cpp/src/main.cc
@@ -1,84 +1,68 @@
 #include <cpr/cpr.h>
-#include <string>
-#include <vector>
+#include <ctime>
+#include <iostream>
 #include <map>
 #include <regex>
-#include <iostream>
-#include <ctime>
+#include <string>
+#include <vector>
+#include <algorithm>
 
+#include "base64.h"
 #include "hmac.h"
 #include "md5.h"
 #include "sha1.h"
 
+#define __DEBUG
 
 using namespace std;
 
-#undef _ALPHA
-#define __DEBUG
-
-const char _PADCHAR = '=';
-const char *_ALPHA =
-    "LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE45QA";
-
-string force(string const &msg) {
-  string ret;
-  for (auto w : ret)
-    ret.push_back(static_cast<uint8_t>(w));
-  return ret;
-}
-
-uint8_t ordat(string const &msg, size_t idx) {
-  return (msg.size() > idx) ? static_cast<uint8_t>(msg[idx]) : 0;
-}
-
-string sencode(string const &msg, bool key) {
-  size_t l = msg.size();
-  string pwd;
-  for (auto i = 0; i < l; i += 4)
-    pwd.push_back(ordat(msg, i) | ordat(msg, i + 1) << 8 |
-                  ordat(msg, i + 2) << 16 | ordat(msg, i + 3) << 24);
-  if (key)
-    pwd.push_back(static_cast<uint8_t>(l));
-  return pwd;
-}
-
-string lencode(string &msg, bool key) {
-  size_t l = msg.size();
-  size_t ll = (l - 1) << 2;
-  if (key) {
-    uint8_t m = msg[l - 1];
-    if (m < ll - 3 || m > ll)
-      return "";
-    ll = static_cast<size_t>(m);
-  }
-  for (auto i = 0; i < l; i++)
-    msg[i] = (static_cast<uint8_t>(msg[i] & 0xFF) +
-              static_cast<uint8_t>((msg[i] >> 8) & 0xFF) +
-              static_cast<uint8_t>((msg[i] >> 16) & 0xFF) +
-              static_cast<uint8_t>((msg[i] >> 24) & 0xFF));
-  string res;
-  if (key)
-    msg.assign(res.begin(), res.begin() + ll);
-  else
-    msg.assign(res.begin(), res.end());
-  return res;
-}
-
-uint8_t _getbyte(string const &s, size_t i) {
-  uint8_t x = static_cast<uint8_t>(s[i]);
-  if (x > 255)
-    throw "INVALID_CHARACTER_ERR: DOM Exception 5";
+const int ordat(string const &msg, size_t idx) { // Test OK
+  if (msg.size() > idx)
+    return static_cast<int>(msg.at(idx));
   return 0;
 }
 
-string get_xencode(string const &msg, string const &key) {
+const vector<int> sencode(string const &msg, bool key) { // Test OK
+  int l = msg.size();
+  vector<int> pwd;
+  for (int i = 0; i < l; i += 4)
+    pwd.push_back(ordat(msg, i) | ordat(msg, i + 1) << 8 |
+                  ordat(msg, i + 2) << 16 | ordat(msg, i + 3) << 24);
+  if (key)
+    pwd.push_back(l);
+  return pwd;
+}
+
+const string lencode(vector<int> &msg, bool key) {  // Test OK
+  int l = msg.size();
+  int ll = (l - 1) << 2;
+  if (key) {
+  int m = msg[l - 1];
+  if (m < ll - 3 || m > ll)
+    return "";
+  ll = m;
+  }
+  string result;
+  for (int i = 0; i < l; i++)
+    result.push_back(static_cast<char>(msg[i] & 0xFF) +
+              static_cast<char>(msg[i] >> 8 & 0xFF) +
+              static_cast<char>(msg[i] >> 16 & 0xFF) +
+              static_cast<char>(msg[i] >> 24 & 0xFF));
+  if (key)
+    return result.substr(0, ll);
+  return result;
+}
+
+const string get_xencode(string const &msg, string const &key) {
   if (msg.empty())
     return "";
   auto pwd = sencode(msg, true);
   auto pwdk = sencode(key, false);
-  if (pwdk.size() < 4)
-    for (auto i = 0; i < 4 - pwdk.size(); i++)
-      pwdk += '0';
+  if (pwdk.size() < 4) {
+    auto len_pwdk = pwdk.size();
+    for (int i = 0; i < 4 - len_pwdk; i++)
+      pwdk.push_back(0);
+  }
   auto n = pwd.size() - 1;
   auto z = pwd[n];
   auto y = pwd[0];
@@ -86,74 +70,51 @@ string get_xencode(string const &msg, string const &key) {
   auto m = 0;
   auto e = 0;
   auto p = 0;
-  auto q = floor(6 + 52 / (n + 1));
+  auto q = 6 + 52 / (n + 1);
   auto d = 0;
-  while (0 < q) {
-    d = d + c & (0x8CE0D9BF | 0x731F2640);
+  while (q > 0) {
+    d += c & (0x8CE0D9BF | 0x731F2640);
     e = d >> 2 & 3;
     p = 0;
     while (p < n) {
       y = pwd[p + 1];
       m = z >> 5 ^ y << 2;
-      m = m + ((y >> 3 ^ z << 4) ^ (d ^ y));
-      m = m + (pwdk[(p & 3) ^ e] ^ z);
-      pwd[p] = pwd[p] + m & (0xEFB8D130 | 0x10472ECF);
+      m += (y >> 3 ^ z << 4) ^ (d ^ y);
+      m += pwdk[(p & 3) ^ e] ^ z;
+      pwd[p] += m & (0xEFB8D130 | 0x10472ECF);
       z = pwd[p];
       p += 1;
     }
     y = pwd[0];
     m = z >> 5 ^ y << 2;
-    m = m + ((y >> 3 ^ z << 4) ^ (d ^ y));
-    m = m + (pwdk[(p & 3) ^ e] ^ z);
-    pwd[n] = pwd[n] + m & (0xBB390742 | 0x44C6F8BD);
+    m += (y >> 3 ^ z << 4) ^ (d ^ y);
+    m += pwdk[(p & 3) ^ e] ^ z;
+    pwd[n] += m & (0xBB390742 | 0x44C6F8BD);
     z = pwd[n];
     q -= 1;
   }
   return lencode(pwd, false);
 }
 
-string get_base64(string const &s) {
-  auto i = 0;
-  auto b10 = 0;
-  vector<uint8_t> x;
-  auto imax = s.size() - s.size() % 3;
-  if (s.empty())
-    return s;
-  for (auto i = 0; i < imax; i += 3) {
-    b10 = ((_getbyte(s, i) << 16) | (_getbyte(s, i + 1) << 8) |
-           _getbyte(s, i + 2));
-    x.push_back(_ALPHA[(b10 >> 18)]);
-    x.push_back(_ALPHA[((b10 >> 12) & 63)]);
-    x.push_back(_ALPHA[((b10 >> 6) & 63)]);
-    x.push_back(_ALPHA[(b10 & 63)]);
-  }
-  i = imax;
-  if (s.size() - imax == 1) {
-    b10 = _getbyte(s, i) << 16;
-    x.push_back(_ALPHA[(b10 >> 18)] + _ALPHA[((b10 >> 12) & 63)] + _PADCHAR +
-                _PADCHAR);
-  } else if (s.size() - imax == 2) {
-    b10 = (_getbyte(s, i) << 16) | (_getbyte(s, i + 1) << 8);
-    x.push_back(_ALPHA[(b10 >> 18)] + _ALPHA[((b10 >> 12) & 63)] +
-                _ALPHA[((b10 >> 6) & 63)] + _PADCHAR);
-  }
-  string res;
-  x.assign(res.begin(), res.end());
-  return res;
+const string &get_md5(string const &password, string const &token) {
+  static string result = hmac<MD5>(password, token);
+  return result;
 }
 
-string get_md5(string const &password, string const &token) {
-  return hmac<MD5>(password, token);
-}
-
-string get_sha1(string const &value) {
+const string &get_sha1(string const &value) {
   SHA1 sha1;
-  return sha1(value);
+  static string result = sha1(value);
+  return result;
+}
+
+const string &get_base64(string const &value) {
+  static string result = base64_encode(value);
+  return result;
 }
 
 cpr::Header header = {
-  {"User-Agent", "Mozilla/5.0 (Windows NT 10.0 WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36"}
-};
+    {"User-Agent", "Mozilla/5.0 (Windows NT 10.0 WOW64) AppleWebKit/537.36 "
+                   "(KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36"}};
 
 string const init_url = "http://10.1.1.131";
 string const get_challenge_api = "http://10.1.1.131/cgi-bin/get_challenge";
@@ -163,13 +124,58 @@ string const n = "200";
 string const type = "1";
 string const ac_id = "1";
 string const enc = "srun_bx1";
-string token;
-string username;
-string password;
+string token = "Unknown";
+string username = "Unknown";
+string password = "Unknown";
 string i;
-string ip;
-string hmd5;
-string chksum;
+string ip = "Unknown";
+string hmd5 = "Unknown";
+string chksum = "Unknown";
+
+void init_getip() {
+  auto init_res = cpr::Get(cpr::Url{init_url.c_str()}, header);
+  regex e("\\d{0,3}\\.\\d{0,3}\\.\\d{0,3}\\.\\d{0,3}");
+  smatch sm;
+  string target = init_res.text;
+#ifdef __DEBUG
+  cout << "initaling ip address..." << endl;
+#endif
+  if (regex_search(target, sm, e))
+    ip = sm[0];
+#ifdef __DEBUG
+  cout << "ip: " << ip << endl;
+#endif
+}
+
+// get current local time stamp
+int64_t getCurrentLocalTimeStamp() {
+  return std::chrono::duration_cast<std::chrono::milliseconds>(
+             std::chrono::system_clock::now().time_since_epoch())
+      .count();
+}
+
+void get_token() {
+#ifdef __DEBUG
+  cout << "getting token..." << endl;
+#endif
+  cpr::Parameters get_challenge_params =
+      cpr::Parameters{{"callback", "jQuery112404953340710317169_" +
+                                       to_string(getCurrentLocalTimeStamp())},
+                      {"username", username},
+                      {"ip", ip},
+                      {"_", to_string(getCurrentLocalTimeStamp())}};
+  auto get_challenge_res = cpr::Get(cpr::Url{get_challenge_api.c_str()},
+                                    get_challenge_params, header);
+  regex e("\"challenge\":\"(.*?)\"");
+  smatch sm;
+  string target = get_challenge_res.text;
+  if (regex_search(target, sm, e))
+    token = sm[1];
+#ifdef __DEBUG
+  cout << "====token info====\n" << target << endl;
+  cout << "token is: " << token << endl;
+#endif
+}
 
 const string get_chksum() {
   auto chkstr = token + username;
@@ -182,62 +188,27 @@ const string get_chksum() {
   return chkstr;
 }
 
-string const get_info() {
-  string info_temp = "{\"username\":" + username +
-                     ",\"password\":" + password + ",\"ip\":" + ip +
-                     ",\"acid\":" + ac_id + ",\"enc_ver\":" + enc + "}";
-  i = info_temp;
-  return i;
-}
-
-void init_getip() {
-  auto init_res = cpr::Get(cpr::Url{init_url.c_str()}, header);
-  regex e("ip(\\s*):(\\s*)\"(.*) \"");
-  smatch sm;
-  string target = init_res.text;
-#ifdef __DEBUG
-  cout << "initaling ip address..." << endl;
-#endif
-  regex_search(target, sm, e);
-  ip = sm[3];
-#ifdef __DEBUG
-  cout << "ip: " << ip << endl;
-#endif
-}
-
-// get current local time stamp
-int64_t getCurrentLocalTimeStamp() {
-  return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-}
-
-void get_token() {
-#ifdef __DEBUG
-  cout << "getting token..." << endl;
-#endif
-  cpr::Parameters get_challenge_params = cpr::Parameters{
-    {"callback", "jQuery112404953340710317169_" + to_string(getCurrentLocalTimeStamp())},
-    {"username", username},
-    {"ip", ip},
-    {"_", to_string(getCurrentLocalTimeStamp())}};
-  auto get_challenge_res = cpr::Get(cpr::Url{get_challenge_api.c_str()},
-                                    get_challenge_params, header);
-  regex e("\"challenge\":\"(.*?)\"");
-  smatch sm;
-  string target = get_challenge_res.text;
-  regex_search(target, sm, e);
-#ifdef __DEBUG
-  cout << target << endl;
-  cout << "token is: " << token << endl;
-#endif
+string const get_info() {   // Test OK
+  string info_temp = "{\"username\":" + username + ",\"password\":" + password +
+                     ",\"ip\":" + ip + ",\"acid\":" + ac_id +
+                     ",\"enc_ver\":" + enc + "}";
+  replace(info_temp.begin(), info_temp.end(), '\'', '\"');
+  info_temp.erase(remove_if(info_temp.begin(), info_temp.end(), ::isspace),
+                  info_temp.end());
+  return info_temp;
 }
 
 void encode() {
   i = get_info();
-    i = "{SRBX1}" + get_base64(get_xencode(i, token));
-    hmd5 = get_md5(password, token);
-    chksum = get_sha1(get_chksum());
+  i = "{SRBX1}" + get_base64(get_xencode(i, token));
+  hmd5 = get_md5(password, token);
+  chksum = get_sha1(get_chksum());
 #ifdef __DEBUG
-    cout << "All encode work done." << endl;
+  cout << "====encode work====" << endl;
+  cout << "i = " << i << endl;
+  cout << "hmd5 = " << hmd5 << endl;
+  cout << "chksum = " << chksum << endl;
+  cout << "All encode work done." << endl;
 #endif
 }
 
@@ -248,22 +219,22 @@ void init_work() {
 }
 
 void login() {
-  auto srun_portal_params = cpr::Parameters{
-    {"callback", "jQuery11240645308969735664_" + to_string(getCurrentLocalTimeStamp())},
-    {"action", "login"},
-    {"username", username},
-    {"password", "{MD5}" + hmd5},
-    {"ac_id", ac_id},
-    {"ip", ip},
-    {"chksum", chksum},
-    {"info", i},
-    {"n", n},
-    {"type", type},
-    {"os", "windows+10"},
-    {"name", "windows"},
-    {"double_stack", "0"},
-    {"_", to_string(getCurrentLocalTimeStamp())}
-  };
+  auto srun_portal_params =
+      cpr::Parameters{{"callback", "jQuery11240645308969735664_" +
+                                       to_string(getCurrentLocalTimeStamp())},
+                      {"action", "login"},
+                      {"username", username},
+                      {"password", "{MD5}" + hmd5},
+                      {"ac_id", ac_id},
+                      {"ip", ip},
+                      {"chksum", chksum},
+                      {"info", i},
+                      {"n", n},
+                      {"type", type},
+                      {"os", "windows+10"},
+                      {"name", "windows"},
+                      {"double_stack", "0"},
+                      {"_", to_string(getCurrentLocalTimeStamp())}};
   auto srun_portal_res =
       cpr::Get(cpr::Url{srun_portal_api}, srun_portal_params, header);
 #ifdef __DEBUG
@@ -284,7 +255,7 @@ void login() {
 int main(int argc, const char **argv) {
   username = "2108570020058";
   password = "snipexl1997";
+
   init_work();
-  login();
   return 0;
 }
